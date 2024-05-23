8e374b12bb9b6c88873f4a171cdce331ff68e0ef
Krantz <totalaj00@hotmail.com>
2024-05-23 17:41:26 +0200

    wip Object Picker
    You can now click objects to select them
    Added extension functions to furnitureobject for translation, scaling, and rotation
    added formatting settings

--- entry.ts
+++ entry.ts
@@ -1,44 +1,143 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, SceneLoader } from "@babylonjs/core";
-import { FurnitureObject } from "./script/furnitureObject"
-
+import {
+	Engine,
+	Scene,
+	ArcRotateCamera,
+	Vector3,
+	HemisphericLight,
+	Mesh,
+	MeshBuilder,
+	SceneLoader,
+	AbstractMesh,
+} from "@babylonjs/core";
+import { FurnitureObject } from "./script/furnitureObject";
 
 class App {
-    
-    private scene : Scene;
-    private engine : Engine;
-    
+	private scene: Scene;
+	private engine: Engine;
 
-    constructor() {
-        // create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "renderCanvas";
-        document.body.appendChild(canvas);
+	private furnitureObjects: FurnitureObject[] = new Array(0);
+	private meshObjectMap: Map<number, FurnitureObject> = new Map();
 
-        // initialize babylon scene and engine
-        this.engine = new Engine(canvas, true);
-        this.scene = new Scene(this.engine);
+	private currentFurnitureObject: FurnitureObject;
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this.scene);
-        camera.attachControl(canvas, true);
+	private header: HTMLElement;
 
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this.scene);
+	constructor() {
+		let self = this;
 
-        var chair : FurnitureObject = new FurnitureObject("models/chair/chair.glb", this.scene);
+		// create the canvas html element and attach it to the webpage
+		var canvas = document.createElement("canvas");
+		canvas.style.width = "100%";
+		canvas.style.height = "100%";
+		canvas.id = "renderCanvas";
+		document.body.appendChild(canvas);
 
-        // run the main render loop
-        this.engine.runRenderLoop(() => {
-            this.scene.render();
-            
-            let dt = this.scene.deltaTime / 1000;
-            dt = !Number.isNaN(dt) ? dt : 0;
+		this.header = document.createElement("h1");
+		this.header.innerText = "Text";
+		document.body.append(this.header);
 
-            chair.loop(dt);
-        });
-    }
+		// initialize babylon scene and engine
+		this.engine = new Engine(canvas, true);
+		this.scene = new Scene(this.engine);
+
+		var camera: ArcRotateCamera = new ArcRotateCamera(
+			"Camera",
+			Math.PI / 2,
+			Math.PI / 2,
+			2,
+			Vector3.Zero(),
+			this.scene
+		);
+		camera.attachControl(canvas, true);
+
+		var light1: HemisphericLight = new HemisphericLight(
+			"light1",
+			new Vector3(1, 1, 0),
+			this.scene
+		);
+
+		// Add testing furniture
+		this.addNewFurniture("models/chair/chair.glb");
+		this.addNewFurniture("models/chair/chair.glb", (loadedObject) => {
+			loadedObject.setPosition(new Vector3(0, 0, 4));
+		});
+		this.addNewFurniture("models/shelf/shelf.glb", (loadedObject) => {
+			loadedObject.setPosition(new Vector3(0, 7, -2));
+		});
+		this.addNewFurniture("models/shelf/shelf.glb", (loadedObject) => {
+			loadedObject.setPosition(new Vector3(0, 7, 0));
+		});
+		this.addNewFurniture("models/shelf/shelf.glb", (loadedObject) => {
+			loadedObject.setPosition(new Vector3(0, 7, 2));
+		});
+
+		this.scene.onPointerDown = function (evt, pickResult) {
+			if (pickResult.hit) {
+				if (self.meshObjectMap.has(pickResult.pickedMesh.uniqueId)) {
+					let pickedFurnitureObject = self.meshObjectMap.get(
+						pickResult.pickedMesh.uniqueId
+					);
+
+					if (self.currentFurnitureObject) {
+						if (
+							!self.currentFurnitureObject.equals(
+								pickedFurnitureObject
+							)
+						) {
+							self.currentFurnitureObject.deselected();
+
+							self.currentFurnitureObject = pickedFurnitureObject;
+							self.currentFurnitureObject.selected();
+						}
+					} else {
+						self.currentFurnitureObject = pickedFurnitureObject;
+						self.currentFurnitureObject.selected();
+					}
+				}
+			} else if (self.currentFurnitureObject != null) {
+				if (self.currentFurnitureObject) {
+					self.currentFurnitureObject.deselected();
+					self.currentFurnitureObject = null;
+				}
+			}
+		};
+
+		// run the main render loop
+		this.engine.runRenderLoop(() => {
+			this.scene.render();
+
+			let dt = this.scene.deltaTime / 1000;
+			dt = !Number.isNaN(dt) ? dt : 0;
+
+			this.furnitureObjects.forEach((furnitureObject) => {
+				furnitureObject.loop(dt);
+			});
+		});
+	}
+
+	addNewFurniture(
+		filename: string,
+		onLoaded?: (loadedObject: FurnitureObject) => void
+	) {
+		var newFurnitureObject: FurnitureObject = new FurnitureObject(
+			filename,
+			this.scene,
+			(loadedFurniture) => {
+				loadedFurniture.meshes.forEach((mesh) => {
+					this.meshObjectMap.set(mesh.uniqueId, loadedFurniture);
+				});
+
+				console.log(this.meshObjectMap);
+				if (onLoaded) {
+					onLoaded(loadedFurniture);
+				}
+				this.furnitureObjects.push(newFurnitureObject);
+			}
+		);
+	}
 }
+
 new App();
--- script/furnitureObject.ts
+++ script/furnitureObject.ts
@@ -1,21 +1,75 @@
 import { AbstractMesh, Scene, SceneLoader, Vector3 } from "@babylonjs/core";
+import { velocityPixelShader } from "@babylonjs/core/Shaders/velocity.fragment";
 
-export class FurnitureObject
-{
-    private mesh : AbstractMesh;
+export class FurnitureObject {
+	public meshes: AbstractMesh[];
 
-    constructor(filename: string, scene: Scene)
-    {
-        var self = this;
-        SceneLoader.ImportMesh(null, "./", filename, scene, function (meshes, particleSystems, skeletons) {
-            self.mesh = meshes.at(0); // Should certainly be checked
-          });
-    }
+	constructor(
+		filename: string,
+		scene: Scene,
+		onLoaded?: (loadedObject: FurnitureObject) => void
+	) {
+		var self = this;
+		//@todo add a callback for failing
+		SceneLoader.ImportMesh(null, "./", filename, scene, function (meshes) {
+			self.meshes = meshes;
 
-    loop(dt : number)
-    {
-        if (this.mesh != null){
-            this.mesh.locallyTranslate(new Vector3(0,dt,0));
-        }
-    }
+			if (onLoaded) {
+				onLoaded(self);
+			}
+		});
+	}
+
+	equals(other: FurnitureObject): boolean {
+		let anyEqual = false;
+		other.meshes.forEach((A) => {
+			this.meshes.forEach((B) => {
+				if (A.uniqueId == B.uniqueId) {
+					anyEqual = true;
+				}
+			});
+		});
+
+		return anyEqual;
+	}
+
+	loop(dt: number) {}
+
+	selected() {
+		console.log("Selected", this.meshes[0].name);
+		this.meshes.forEach((mesh) => {
+			mesh.locallyTranslate(new Vector3(0, 0.3, 0));
+		});
+	}
+
+	deselected() {
+		console.log("Deselected", this.meshes[0].name);
+		this.meshes.forEach((mesh) => {
+			mesh.locallyTranslate(new Vector3(0, -0.3, 0));
+		});
+	}
+
+	locallyTranslate(vector3: Vector3) {
+		this.meshes.forEach((mesh) => {
+			mesh.locallyTranslate(vector3);
+		});
+	}
+
+	setPosition(position: Vector3) {
+		this.meshes.forEach((mesh) => {
+			mesh.position = position;
+		});
+	}
+
+	setRotation(rotation: Vector3) {
+		this.meshes.forEach((mesh) => {
+			mesh.rotation = rotation;
+		});
+	}
+
+	setScaling(scaling: Vector3) {
+		this.meshes.forEach((mesh) => {
+			mesh.scaling = scaling;
+		});
+	}
 }
